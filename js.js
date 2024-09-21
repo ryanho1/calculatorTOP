6ae8eb972ff7b3f6bc1a9f827298036ad9456314
ryanho1 <ryan81645@gmail.com>
2024-09-21 18:03:58 +0800

    coded first version addition operation for single numbers

--- js.js
+++ js.js
@@ -8,32 +8,51 @@
     // clear.addEventListener('click', () => {
     //     screen.value = '';
     // })
-  
+
+    let input1 = Number;
+    let input2 = Number;
+    let userInput = "8+6";
+    const twoPlusFiveObj = Array.from(userInput);
+    const filteredNumberStrings = twoPlusFiveObj.filter(Number);
+    const filteredNumberIntegers = filteredNumberStrings.map(Number);
+    console.log(filteredNumberIntegers);
+
   const add = function(input1,input2) {
     let newValue = input1 + input2;
     return newValue;
   };
 
-  console.log(add(3,5));
-  
   const subtract = function(input1,input2) {
     let newValue = input1 - input2;
     return newValue;
   };
-
-  console.log(subtract(3,5));
   
   const multiply = function(input1,input2) {
   let newValue = input1 * input2;
   return newValue;
   };
 
-  console.log(multiply(3,5));
-
   const divide = function(input1,input2) {
     let newValue = input1 / input2;
     return newValue;
   };
 
-  console.log(divide(3,2))
-  
+  //testing with reduce
+  const addingTEST = function(filteredNumberIntegers) {
+    let newValue = filteredNumberIntegers.reduce(function(total, num) {return total + num}, 0);
+    return newValue;
+    };
+
+function operate(userInput) {
+  if (userInput.includes("+")){
+   return addingTEST(filteredNumberIntegers);
+  }
+}  
+
+console.log(operate(userInput))
+
+//get user input
+// filter it by number
+// create a new array with filtered numbers, then put those into the parameters
+// then scan if user input has "+,-,x,/"
+// then call function based on that
