75f577fd00966e3d3ec299673054e8af1a5725e2
ryanho1 <ryan81645@gmail.com>
2024-09-22 04:27:28 +0800

    made buttons appear on display without onlick function in html directly

--- js.js
+++ js.js
@@ -1,24 +1,33 @@
-    // const screen = document.getElementById("screen");
-    // const clear = document.getElementById("delete");
+    const screen = document.getElementById("screen");
+    const clear = document.getElementById("delete");
+    const calculate = document.getElementById("calculate");
+    const keys = document.querySelectorAll('.keys');
+    let userInput = "";
 
-    // function inputOnScreen(input) {
-    //     screen.value += input;
-    // }
+    keys.forEach(button => button.addEventListener('click',() => {
+      let buttonContent = button.textContent;
+      screen.value += buttonContent;
+  }));
 
-    // clear.addEventListener('click', () => {
-    //     screen.value = '';
-    // })
+      calculate.addEventListener('click', () => {
+      userInput = screen.value;
 
-    let input1 = Number;
-    let input2 = Number;
+    clear.addEventListener('click', () => {
+        screen.value = '';
+    })
+//})
+//}
+    //create a userInput with input, so add the strings together (7 + 7) then boom
+    //then return function on screen.value im going to assume
 
-    let userInput = "2+2-1+5";
     const userInputObjectForm = userInput.split(/[+-/*]/);
-    console.log(userInputObjectForm);
+    console.log(userInput);
+    // console.log(userInputObjectForm);
     const filteredNumberStrings = userInputObjectForm.filter(Number);
     const filteredNumberIntegers = filteredNumberStrings.map(Number);
-    console.log(filteredNumberIntegers);
+    // console.log(filteredNumberIntegers);
 
+    //operand logic
   const subtract = function(filteredNumberIntegers) {
     if (filteredNumberIntegers.length === 0) return 0;
     let newValue = filteredNumberIntegers.reduce(function(total, num) {return total - num});
@@ -35,16 +44,14 @@
     return newValue;
   };
 
-  // do reduce function for all operand functions, then test with /[+-/*]/
-
   const add = function(filteredNumberIntegers) {
-    let newValue = filteredNumberIntegers.reduce(function(total, num) {return total + num});
+    let newValue = filteredNumberIntegers.reduce(function(total, num) {return total + num}, 0);
     return newValue;
     };
 
 function operate(userInput) {
   if (userInput.includes('/')) {
-    return divide(filteredNumberIntegers)
+    return divide(filteredNumberIntegers) // something here, perhaps make it += or something like that with screen.value (assign function to var, then += with said var)
   } else if (userInput.includes('*')) {
     return multiply(filteredNumberIntegers)
   } else if (userInput.includes("-")) {
@@ -52,12 +59,13 @@ function operate(userInput) {
   } else if (userInput.includes("+")){
     return add(filteredNumberIntegers);
    }
-}
+};
 
-console.log(operate(userInput))
+console.log(operate(userInput));
+})
 
-//get user input
-// filter it by number
-// create a new array with filtered numbers, then put those into the parameters
-// then scan if user input has "+,-,x,/"
-// then call function based on that
+
+
+
+// bug: doesnt work with multiple operators
+// 
